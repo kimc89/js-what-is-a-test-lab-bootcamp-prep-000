e4472665cdf4515461f3e5e0167ca0cec94a36dd
Kimberly Cushing <kimcushing89@gmail.com>
2017-11-27 18:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,2 @@
 // write your code below!
-`var index = require("../index.js")
 
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
-
-
-  describe("Height", () => {
-    describe('height is less than 40', () => {
-      it('returns "${height}"')
-      expect(index.height).toBeLessThan(40)
-    })
-  })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
-
-}
